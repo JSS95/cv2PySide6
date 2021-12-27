4368214ed8a1e6572e19da739bb8f01d0d6adc23
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-12-28 03:45:22 +0900

    feat : introduce videoplayer.py
    Add videoplayer.py and test

--- cv2PySide6/__init__.py
+++ cv2PySide6/__init__.py
@@ -3,6 +3,8 @@ from .version import __version__ # noqa
 
 from .utilwidgets import ClickableSlider
 from .labels import ScalableQLabel, NDArrayLabel, array2qimage, qimage2array
+from .videoplayer import (QVideoFrame2Array, NDArrayVideoWidget,
+    NDArrayVideoPlayerWidget)
 from .util import get_samples_path
 
 
@@ -13,6 +15,10 @@ __all__ = [
     "NDArrayLabel",
     "array2qimage",
     "qimage2array",
+
+    "QVideoFrame2Array",
+    "NDArrayVideoWidget",
+    "NDArrayVideoPlayerWidget",
 
     "get_samples_path",
 ]
