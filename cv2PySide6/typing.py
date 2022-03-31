320ac02553087babd79f7e8e873fef6c1cdf861e
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-03-31 20:33:10 +0900

    feat: introduce VideoPlayerProtocol
    Remove unnecessary protocol

--- cv2PySide6/typing.py
+++ cv2PySide6/typing.py
@@ -1,12 +1,9 @@
 from numpy.typing import NDArray
 from PySide6.QtCore import Signal
-from PySide6.QtMultimedia import QMediaPlayer
 from typing import Protocol, Callable, Any
 
 __all__ = [
     'ArrayProcessorProtocol',
-    'VideoSeekControllerProtocol',
-    'VideoPlayControllerProtocol',
 ]
 
 
@@ -19,44 +16,3 @@ class ArrayProcessorProtocol(Protocol):
     """
     setArray: Callable[['ArrayProcessorProtocol', NDArray], Any]
     arrayChanged: Signal
-
-
-class VideoSeekControllerProtocol(Protocol):
-    """
-    Type annotation for video seeking controller protocol.
-
-    ``setPosition`` is a ``Slot`` which controls the position
-    of video. When video position changes, ``positionChanged`` signal
-    emits integer value.
-
-    When total duration of the video changes (i.e. new video is loaded),
-    ``durationChanged`` signal emits integer value.
-    """
-    setPosition: Callable[['VideoSeekControllerProtocol', int], Any]
-    positionChanged: Signal
-    durationChanged: Signal
-
-
-class VideoPlayControllerProtocol(Protocol):
-    """
-    Type annotation for video playing controller protocol.
-
-    ``play``, ``pause``, and ``stop`` are ``Slot`` to control the
-    playback state of video. When playback state changes,
-    ``playbackStateChanged`` signal emits new playback state as
-    ``QMediaPlayer.PlaybackState`` enums.
-
-    Notes
-    ====
-
-    This protocol does not necessarily produce video stream, but
-    controls playback state of video producer.
-
-    """
-    play: Callable[['VideoPlayControllerProtocol'], Any]
-    pause: Callable[['VideoPlayControllerProtocol'], Any]
-    stop: Callable[['VideoPlayControllerProtocol'], Any]
-    playbackStateChanged: Signal
-    playbackState: Callable[
-        ['VideoPlayControllerProtocol'], QMediaPlayer.PlaybackState
-    ]
