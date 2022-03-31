c96b917964d947621353f3f34fe919fc90657869
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-03-31 18:59:45 +0900

    feat: introduce VideoPlayerProtocol
    Introduce VideoPlayerProtocol

--- cv2PySide6/typing.py
+++ cv2PySide6/typing.py
@@ -3,7 +3,9 @@ from PySide6.QtCore import Signal
 from typing import Protocol, Callable, Any
 
 __all__ = [
+    'ArrayProcessorProtocol',
     'VideoSeekerProtocol',
+    'VideoPlayerProtocol',
 ]
 
 
@@ -12,7 +14,7 @@ class ArrayProcessorProtocol(Protocol):
     Type annotation for array processor protocol.
 
     New image array is passed down from upstream pipeline to
-    :attr:`setArray` slot. :attr:`arrayChanged` emits processed array.
+    ``setArray`` slot. ``arrayChanged`` emits processed array.
     """
     setArray: Callable[['ArrayProcessorProtocol', NDArray], Any]
     arrayChanged: Signal
@@ -22,13 +24,28 @@ class VideoSeekerProtocol(Protocol):
     """
     Type annotation for video seeker protocol.
 
-    :attr:`setPosition` is a ``Slot`` which controls the position
-    of video. When video position changes, `positionChanged` signal
+    ``setPosition`` is a ``Slot`` which controls the position
+    of video. When video position changes, ``positionChanged`` signal
     emits integer value.
 
     When total duration of the video changes (i.e. new video is loaded),
-    `durationChanged` signal emits integer value.
+    ``durationChanged`` signal emits integer value.
     """
     setPosition: Callable[['VideoSeekerProtocol', int], Any]
     positionChanged: Signal
     durationChanged: Signal
+
+
+class VideoPlayerProtocol(Protocol):
+    """
+    Type annotation for video player protocol.
+
+    ``play``, ``pause``, and ``stop`` are ``Slot`` to control the
+    playback state of video. When playback state changes,
+    ``playbackStateChanged`` signal emits new playback state as
+    ``QMediaPlayer.PlaybackState`` enums.
+    """
+    play: Callable[['VideoPlayerProtocol'], Any]
+    pause: Callable[['VideoPlayerProtocol'], Any]
+    stop: Callable[['VideoPlayerProtocol'], Any]
+    playbackStateChanged: Signal
