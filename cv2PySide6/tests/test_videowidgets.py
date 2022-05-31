dbc5a78b4b5aa547a7fd46eec4d7bf6a59edd3f9
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-01 02:23:15 +0900

    feat: introduce VideoController
    Add tests

--- cv2PySide6/tests/test_videowidgets.py
+++ cv2PySide6/tests/test_videowidgets.py
@@ -1,8 +1,8 @@
 from cv2PySide6 import (
     get_data_path,
     ClickableSlider,
+    MediaController,
     NDArrayVideoPlayerWidget,
-    ScalableQLabel,
 )
 from PySide6.QtCore import Qt, QPoint, QUrl
 from PySide6.QtMultimedia import QMediaPlayer
@@ -14,19 +14,84 @@ VID_PATH = get_data_path("hello.mp4")
 def test_ClickableSlider(qtbot):
     slider = ClickableSlider()
     qtbot.addWidget(slider)
-
     assert slider.value() == 0
 
     pos = QPoint(10, 10)
     qtbot.mouseClick(slider, Qt.LeftButton, pos=pos)
-
     assert slider.value() == slider.pixelPosToRangeValue(pos)
 
 
-def test_NDArrayVideoPlayerWidget_openfile(qtbot):
-    vpwidget = NDArrayVideoPlayerWidget()
-    vpwidget.videoLabel().setPixmapScaleMode(ScalableQLabel.PM_NoScale)
-    vpwidget.videoPlayer().setSource(QUrl.fromLocalFile(VID_PATH))
+def test_MediaController_playpausestop(qtbot):
+    controller = MediaController()
+    player = QMediaPlayer()
+    controller.setPlayer(player)
+    player.setLoops(QMediaPlayer.Infinite)
+    player.setSource(QUrl.fromLocalFile(VID_PATH))
 
-    # opening the video does not play it
-    assert vpwidget.videoPlayer().playbackState() == QMediaPlayer.StoppedState
+    with qtbot.waitSignal(
+        player.playbackStateChanged,
+        check_params_cb=lambda state: state == QMediaPlayer.PlayingState,
+    ):
+        qtbot.mouseClick(controller.playButton(), Qt.LeftButton)
+
+    with qtbot.waitSignal(
+        player.playbackStateChanged,
+        check_params_cb=lambda state: state == QMediaPlayer.PausedState,
+    ):
+        qtbot.mouseClick(controller.playButton(), Qt.LeftButton)
+
+    with qtbot.waitSignal(
+        player.playbackStateChanged,
+        check_params_cb=lambda state: state == QMediaPlayer.StoppedState,
+    ):
+        qtbot.mouseClick(controller.stopButton(), Qt.LeftButton)
+
+
+def test_MediaController_pausedBySliderPress(qtbot):
+    controller = MediaController()
+    player = QMediaPlayer()
+    controller.setPlayer(player)
+    player.setLoops(QMediaPlayer.Infinite)
+    player.setSource(QUrl.fromLocalFile(VID_PATH))
+
+    pos = QPoint(10, 10)
+    qtbot.mousePress(controller.slider(), Qt.LeftButton, pos=pos)
+    assert player.playbackState() == QMediaPlayer.StoppedState
+    qtbot.mouseRelease(controller.slider(), Qt.LeftButton)
+    assert player.playbackState() == QMediaPlayer.StoppedState
+
+    player.play()
+    pos = QPoint(20, 20)
+    qtbot.mousePress(controller.slider(), Qt.LeftButton, pos=pos)
+    assert player.playbackState() == QMediaPlayer.PausedState
+    qtbot.mouseRelease(controller.slider(), Qt.LeftButton)
+    assert player.playbackState() == QMediaPlayer.PlayingState
+
+    player.pause()
+    pos = QPoint(10, 10)
+    qtbot.mousePress(controller.slider(), Qt.LeftButton, pos=pos)
+    assert player.playbackState() == QMediaPlayer.PausedState
+    qtbot.mouseRelease(controller.slider(), Qt.LeftButton)
+    assert player.playbackState() == QMediaPlayer.PausedState
+
+
+def test_MediaController_slider_range(qtbot):
+    controller = MediaController()
+    player = QMediaPlayer()
+    controller.setPlayer(player)
+
+    with qtbot.waitSignal(controller.slider().rangeChanged):
+        player.setSource(QUrl.fromLocalFile(VID_PATH))
+
+
+def test_NDArrayVideoPlayerWidget(qtbot):
+    vpwidget = NDArrayVideoPlayerWidget()
+    vpwidget.videoPlayer().setSource(QUrl.fromLocalFile(VID_PATH))
+    vpwidget.videoPlayer().setPlaybackRate(100)
+    with qtbot.waitSignal(
+        vpwidget.videoPlayer().playbackStateChanged,
+        check_params_cb=lambda state: state == QMediaPlayer.StoppedState,
+        timeout=None,
+    ):
+        qtbot.mouseClick(vpwidget.videoController().playButton(), Qt.LeftButton)
+    assert not vpwidget.videoLabel().pixmap().isNull()
