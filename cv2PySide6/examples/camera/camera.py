8b53bf9b5cf082e7eedcbc9dce632a8ae1fbcc65
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-03 10:27:16 +0900

    doc: make camera example based on multithreading
    Implement ArraySender

--- cv2PySide6/examples/camera/camera.py
+++ cv2PySide6/examples/camera/camera.py
@@ -4,29 +4,36 @@ import cv2  # type: ignore
 from cv2PySide6 import NDArrayMediaCaptureSession, NDArrayLabel
 import numpy as np
 import numpy.typing as npt
-from PySide6.QtCore import QObject, Signal, Qt
+from PySide6.QtCore import QObject, Signal, Slot, Qt
 from PySide6.QtWidgets import QMainWindow
 
 
 class BlurringProcessor(QObject):
-    """
-    Video pipeline component for Gaussian blurring on numpy array.
-    """
+    """Video pipeline component for Gaussian blurring on numpy array."""
 
     arrayChanged = Signal(np.ndarray)
 
+    @Slot(np.ndarray)
     def setArray(self, array: npt.NDArray[np.uint8]):
         self.arrayChanged.emit(cv2.GaussianBlur(array, (0, 0), 25))
+
+
+class ArraySender(QObject):
+    """Object to sent the array to :class:`BlurringProcessor`."""
+
+    arrayChanged = Signal(np.ndarray)
 
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._captureSession = NDArrayMediaCaptureSession()
+        self._arraySender = ArraySender()
         self._arrayProcessor = BlurringProcessor()
         self._arrayLabel = NDArrayLabel()
 
-        self.captureSession().arrayChanged.connect(self.arrayProcessor().setArray)
+        self.captureSession().arrayChanged.connect(self.onArrayPassedFromCamera)
+        self._arraySender.arrayChanged.connect(self.arrayProcessor().setArray)
         self.arrayProcessor().arrayChanged.connect(self.arrayLabel().setArray)
         self.arrayLabel().setAlignment(Qt.AlignCenter)  # type: ignore[arg-type]
         self.setCentralWidget(self.arrayLabel())
@@ -43,6 +50,10 @@ class Window(QMainWindow):
 
     def arrayLabel(self) -> NDArrayLabel:
         return self._arrayLabel
+
+    @Slot(np.ndarray)
+    def onArrayPassedFromCamera(self, array: npt.NDArray[np.uint8]):
+        self._arraySender.arrayChanged.emit(array)
 
 
 if __name__ == "__main__":
