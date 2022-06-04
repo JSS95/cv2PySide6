28e2fc02bb9ede639cc5304740c5dac6b7fb3482
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-04 13:22:38 +0900

    doc: add sphinx.ext.viewcode

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,10 +9,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../cv2PySide6'))
+
 autodoc_mock_imports = ["PySide6"]
 
 
@@ -31,6 +32,7 @@ author = "Jisoo Song"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
     "numpydoc",
 ]
 
